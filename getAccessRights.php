9b7b605bcd0f786cb5fbdc40bc854cb716742d8d
Subzidion <subzidion@outlook.com>
2015-07-25 01:01:01 -0400

    Updated AccessRights and reigstrarRequest
    registrarRequest now correctly connects to database. dbname was case
    sensitive. AccessRights had slight errors in queries and PDO statements
    that were updated.

--- getAccessRights.php
+++ getAccessRights.php
@@ -6,13 +6,22 @@
     //Use Database Connection variable in registrarRequest
     global $dbConn;
     //Prepare query
-    $query = "SELECT personnel.accessrights AS accessRights WHERE personnel.ID = :uuid";
+    $query = "SELECT accounts.PID AS PID FROM accounts WHERE accounts.UUID = :uuid";
     //Prepare Statement
     $statement = $dbConn->prepare($query);
     //Bind parameter to query
-    $statement->bindValue(':uuid', strval($uuid), PDO::PARAM_STRING);
+    $statement->bindValue(':uuid', strval($uuid), PDO::PARAM_STR);
+    if(!$statement->execute()) throw new Exception("Query failed: " . $statement->errorInfo()[2] .".");
+    //Fetch result
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    //Prepare query
+    $query = "SELECT personnel.accessrights AS accessRights FROM personnel WHERE personnel.ID = :pid";
+    //Prepare Statement
+    $statement = $dbConn->prepare($query);
+    //Bind parameter to query
+    $statement->bindValue(':pid', intval($result['PID']), PDO::PARAM_STR);
     //Execute, throw exception if query fails
-    if(!$statement->execute()) throw new Exception("Query failed.");
+    if(!$statement->execute()) throw new Exception("Query failed: " . $statement->errorInfo()[2] .".");
     //Fetch result
     $result = $statement->fetch(PDO::FETCH_ASSOC);
     //Create array of data to return
--- registrarRequest.php
+++ registrarRequest.php
@@ -1,35 +1,29 @@
 <?php
   //Include the SecretKey
-  include("../../overwatch/info.php");
+  include "../../overwatch/info.php";
   //Include Database Information
-  include("../../external_includes/mysql_network_config.php");
-  include("getPID.php");
-  include("getUsername.php");
-  include("getAccessRights.php");
-  include("getGeneraiton.php");
-  include("getDivision.php");
-  include("getRank.php");
-  include("getPersonnel.php");
-  include("getAccounts.php");
+  include "../../external_includes/mysql_network_config.php";
+  include "getAccessRights.php";
 
   function errorReport($message) {
     die("ERROR: " . $message);
   }
 
   //AUTHENTICATE REQUEST
-  if(!isset($_POST['key']) || $_POST['key'] !== $secretKey) error("Access denied.");
-  if(!isset($_SERVER['HTTP_X_SECONDLIFE_SHARD'])) error("Access denied.");
+  if(!isset($_POST['key']) || $_POST['key'] !== $secretKey) errorReport("Access denied.");
+  if(!isset($_SERVER['HTTP_X_SECONDLIFE_SHARD'])) errorReport("Access denied.");
 
   //Connect to Database. Need to determine if requesting user is authorized
   try {
-    $dbConn = new PDO("mysql:host=$dbServer;dbName=$dbName", $dbUser, $dbPass);
+    $dbConn = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPassword);
   } catch(PDOException $e) {
-    error("Database connection failed.");
-  }
-  
+    errorReport("Database connection failed.");
+  } 
+
   $requestUUID = $_SERVER["HTTP_X_SECONDLIFE_OWNER_KEY"];
-
-  if(count(getAccessRightsFromUUID($requestUUID)) == 0) error("Access denied.");
-
-  $dbConn = null;
+  try {
+    if(count(getAccessRightsFromUUID($requestUUID)) == 0) errorReport("Access denied.");
+  } catch(Exception $e) {
+    errorReport($e->getMessage());
+  }
 ?>    
